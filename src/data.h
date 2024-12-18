6de67488a7d235411e3e86b99980e4770b5c49c6
Danil Komarov <daniil234agentfinger.monforje@gmail.com>
2024-12-18 13:15:34 +1000

    data crafting
    made data.h data.cpp, started to make link with rb tree key and txt file

---
+++ src/data.h
@@ -0,0 +1,45 @@
+#ifndef DATA_H
+#define DATA_H
+
+#include <string>
+#include <fstream>
+
+struct KEY {
+    int series;
+    int number;
+};
+
+class ReadFromFile() {
+    void file(const std::string& filename) {
+    std::ifstream inputFile(filename);
+    if (!inputFile) {
+        std::cerr << "Ошибка открытия файла!" << std::endl;
+        return;
+    }
+
+    int line, lineNumber = 0;
+
+    while (std::getline(inputFile, line)) {
+        lineNumber++;
+        std::istringstream iss(line);
+        int series, number;
+
+        if (!(iss >> series >> number)) {
+            std::cerr << "Ошибка формата в строке " << lineNumber << ": " << line << std::endl;
+            continue;
+        }
+
+            if ((series < 1000 || series > 9925) || (number < 100000 || 999999)) {
+                std::cerr << "Ошибка: Номер группы должен быть четырёхзначным числом в строке: " << lineNumber << std::endl;
+                continue;
+            }
+        } else {
+            std::cerr << "Ошибка чтения строки: " << lineNumber << std::endl;
+        }
+
+    }
+
+    inputFile.close();
+}
+
+#endif
--- src/red-black.cpp
+++ src/red-black.cpp
@@ -1,28 +1,20 @@
+#include <iostream>
 #include "src/list.h"
 
-int main() {
-    DoublyLinkedList dll;
+struct KEY {
+    int data;
+    DoublyLinkedList list;
+};
 
-    dll.push_back("1");
-    dll.push_back("2");
-    dll.push_back("3");
-    dll.push_back("2");
-    dll.push_back("4");
+enum COLOR { red, black };
 
-    std::cout << "Initial list: ";
-    dll.print_forward();
-
-    dll.remove("2");
-    std::cout << "After removing the first 2: ";
-    dll.print_forward();
-
-    dll.remove("4");
-    std::cout << "After removing 4: ";
-    dll.print_forward();
-
-    dll.clear();
-    std::cout << "After clearing the list: ";
-    dll.print_forward();
-
-    return 0;
-}
+class RBtree {
+private:
+    struct RBnode { 
+        KEY key;
+        COLOR color;
+        RBnode* left;
+        RBnode* right;
+        RBnode* parent;
+    };
+};
