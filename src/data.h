ca4490a39a6daed50efea596e60462aa8f32843d
Danil Komarov <daniil234agentfinger.monforje@gmail.com>
2024-12-20 20:04:34 +1000

    edited file

--- src/data.h
+++ src/data.h
@@ -6,19 +6,15 @@
 #include <fstream>
 #include <sstream>
 
-// Структура для хранения данных
 struct Data {
     int series;
     int number;
 };
 
-// Константа для максимального размера массива
 const int SIZE = 100;
 
-// Класс для чтения данных из файла
 class ReadFromFile {
 public:
-    // Метод для чтения данных из файла
     void readFile(const std::string& filename, Data* dataArray, int& count);
 };
 
